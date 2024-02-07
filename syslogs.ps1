45bcf95413c74f4811c19a39ae0441d765bbd631
drcluster <30862420+drcluster@users.noreply.github.com>
2024-02-07 23:37:17 +0200

    Add files via upload

--- syslogs.ps1
+++ syslogs.ps1
@@ -1,13 +1,10 @@
-# Request user input for the beginning and ending dates, and the file save location
 $initialDate = Read-Host "Input the initial date (format: 'MM/DD/YYYY')"
 $finalDate = Read-Host "Input the final date (format: 'MM/DD/YYYY')"
 $savePath = Read-Host "Specify the file save location (example: 'C:\path\to\output.txt')"
 
-# Convert the input strings to DateTime objects
 $startDate = [datetime]::ParseExact($initialDate, 'MM/dd/yyyy', $null)
 $endDate = [datetime]::ParseExact($finalDate, 'MM/dd/yyyy', $null)
 
-# Retrieve System logs within the specified date range, filtering for certain severity levels
 $systemLogs = Get-WinEvent -FilterHashtable @{
     LogName = 'System'
     StartTime = $startDate
@@ -15,7 +12,6 @@ $systemLogs = Get-WinEvent -FilterHashtable @{
     Level = @(1, 2, 3) 
 } -ErrorAction SilentlyContinue
 
-# Organize and sort the logs by ProviderName, then by the number of entries, and format the output
 $sortedLogs = $systemLogs | Group-Object { $_.ProviderName } | 
               Sort-Object Count -Descending | 
               ForEach-Object {
@@ -29,13 +25,10 @@ $sortedLogs = $systemLogs | Group-Object { $_.ProviderName } |
                   ($logEntries | Format-Table -HideTableHeaders | Out-String).Trim()
               }
 
-# Check if the directory exists before saving the file, if not, create it
 If (!(Test-Path -Path $(Split-Path -Path $savePath -Parent))) {
     New-Item -ItemType Directory -Force -Path $(Split-Path -Path $savePath -Parent)
 }
 
-# Output the sorted and formatted logs to the specified file
 $sortedLogs | Out-File -FilePath $savePath
 
-# Confirm file save location to the user
 Write-Host "System logs have been stored at: $savePath"
